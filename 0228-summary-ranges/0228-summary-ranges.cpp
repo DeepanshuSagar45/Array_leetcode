dd83a7d8b52efaa8db11d418dcbe6bd66fb8ee26
Deepanshu sagar <deepsagar7060811049@gmail.com>
2026-09-05 22:32:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (9.53%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -3,20 +3,33 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
 
-        if (nums.empty()) return ans;
+        int n = nums.size();
+
+        if (n == 0)
+            return ans;
 
         int start = nums[0];
 
-        for (int i = 1; i <= nums.size(); i++) {
-            if (i == nums.size() || nums[i] != nums[i - 1] + 1LL) {
+        for (int i = 1; i <= n; i++) {
+
+            // End of a consecutive range
+            if (i == n || (long long)nums[i] != (long long)nums[i - 1] + 1) {
+
+                // Single number
                 if (start == nums[i - 1]) {
                     ans.push_back(to_string(start));
-                } else {
-                    ans.push_back(to_string(start) + "->" + to_string(nums[i - 1]));
+                }
+                // Range
+                else {
+                    ans.push_back(
+                        to_string(start) + "->" + to_string(nums[i - 1])
+                    );
                 }
 
-                if (i < nums.size())
+                // Start the next range
+                if (i < n) {
                     start = nums[i];
+                }
             }
         }
 
