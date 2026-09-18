55051b1fcdc197169167cc5226d24d5f53ee7069
Deepanshu sagar <deepsagar7060811049@gmail.com>
2026-09-18 20:54:21 +0530

    Time: 74 ms (34.27%), Space: 82.5 MB (43.36%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> current;
+
+    void solve(int start, int n, int k) {
+        if (current.size() == k) {
+            ans.push_back(current);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            current.push_back(i);
+
+            solve(i + 1, n, k);
+
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        solve(1, n, k);
+        return ans;
+    }
+};
